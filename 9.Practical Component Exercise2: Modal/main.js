e7fb88858c6dcc82c7f7e764221465b465973977
Wasid Hossain <wasidmail@gmail.com>
2017-06-18 09:07:03 +0000

    Component Communication Example 1: Custom Events

--- 9.Practical Component Exercise2: Modal/main.js
+++ 9.Practical Component Exercise2: Modal/main.js
@@ -1,78 +1,45 @@
-Vue.component('tabs', {
+Vue.component('coupon', {
 
     template: `
     
         <div>
-            <div class="tabs">
-              <ul>
-                <li v-for="singletab in alltabs" :class="{ 'is-active' : singletab.isActive }">
-                    <a :href="singletab.href" @click="clicked(singletab)">{{ singletab.name }}</a>
-                </li>
-              </ul>
-            </div>
-            
-            <div class="tab-details">
-                <slot></slot>
-            </div>
+            <input placeholder="enter coupon" @blur="testCoupon" @focus="removeMsg">
         </div>
     `,
     
-    data(){
-       
-       return{ alltabs: [] };
-        
-    },
     
     methods: {
-        clicked(selectedtab){ // @click="clicked(singletab)
-            this.alltabs.forEach(single_tab => {
-                single_tab.isActive = (single_tab.name == selectedtab.name);
-            });
-        }
-    },
-    
-    created() {
-        this.alltabs = this.$children;
+        testCoupon(){
+           this.$emit('applied');
+       },
+       
+       removeMsg(){
+           this.$emit('removed');
+       },
     }
 });
 
-Vue.component('tab', {
-    template: `
-        <div  v-if="isActive">
-            <slot></slot>
-        </div>
-    `,
-    props: {
-       
-       name: { reqired: true },
-       
-       selected: { default: false }
-       
-    },
-    
-    data() {
-        return {
-            isActive: false
-        }
-        
-    },
-    
-    computed: {
-        
-        href() {
-            return '#' + this.name.toLowerCase().replace(/ /g, '-');
-        }
-    },
-    
-    mounted() {
-        this.isActive = this.selected
-    }
-    
-  
-    
-});
+
 
 let app = new Vue({
-   el: '#vueApp10',
+   
+   el: '#vueApp11',
+   
+   data: {
+       
+       validCouponApplied: false
+        
+    },
+   
+   methods: {
+       couponApplied(){
+           this.validCouponApplied = true
+       },
+       
+       couponRemoved(){
+           this.validCouponApplied = false
+       },
+       
+   }
        
 });
