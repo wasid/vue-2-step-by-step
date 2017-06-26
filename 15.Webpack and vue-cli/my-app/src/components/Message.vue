30930797dad5fee31c101bc99060db3bbec124c5
Wasid Hossain <wasidmail@gmail.com>
2017-06-26 20:23:40 +0000

    Webpack and vue-cli part 2

---
+++ 15.Webpack and vue-cli/my-app/src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="box">
+        <p>
+          <slot></slot>  
+        </p>
+    </div>
+</template>
+
+<script>
+    export default{
+        
+    }
+</script>
+
+<style>
+    .box{
+        background:steelblue;
+        padding:10px;
+        border:1px solid #c5c5c5;
+        width: 50%;
+        color: white;
+        margin-bottom:10px;
+    }
+</style>
